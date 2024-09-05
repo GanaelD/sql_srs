5d8f704ebfc2fc6d9541f5304266d745c2e53629
GanaelD <ganael59320@gmail.com>
2024-09-05 20:06:14 +0200

    add theme selection

--- app.py
+++ app.py
@@ -3,7 +3,20 @@ import pandas as pd
 import duckdb
 from typing import Optional
 
-st.write("Hello world!")
+st.write("""
+# SQL SRS
+Spaced Repetition System SQL practice
+""")
+
+option = st.selectbox(
+    "What would you like to review?",
+    ("Joins", "GroupBy", "Window Functions"),
+    index=None,
+    placeholder="Select a theme..."
+)
+
+st.write(f"You selected: {option}")
+
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
